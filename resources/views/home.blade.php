7f83e2d9f127e8d55d6b792fe721fd4f7cb92d6b
Marzia Fatarella <marziafatarella@gmail.com>
2020-07-01 12:40:35 +0200

    jumbo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h1 class="text-center">Benvenuti!</h1>
+    <section id="boolean-jumbo">
+        <div class="container">
+            <div class="row">
+                <div class="my-jumbo col-md-6">
+                    <h1 class="my-jumbo-title">
+                        diventa <strong>sviluppatore web</strong>
+                    </h1>
+                    <p>
+                        Trasformiamo la tua passione in una carriera. Se non trovi lavoro, ti rimborsiamo.
+                    </p>
+                    <ul>
+                        <li>
+                            <span>6 mesi</span> di corso intensivo online in diretta
+                        </li>
+                        <li>
+                            <span>Nessuna competenza</span> di programmazione richiesta
+                        </li>
+                        <li>
+                            Siamo certi del tuo successo, altrimenti <span>ti rimborsiamo</span>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-6"></div>
             </div>
         </div>
-    </div>
+    </section>
 @endsection
 
 @section('page-title', 'Homepage')
--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -5,28 +5,28 @@
                 <img src="https://www.boolean.careers/images/common/logo.png" alt="Boolean Careers">
             </a>
             <div class="boolean__navbar__menu">
-                <ul class="navbar-nav">
+                <ul class="menu">
                     <li class="boolean__navbar__menu active">
                         <a class="nav-link" href="{{ route('homepage') }}"">Home</a>
                     </li>
-                    <li class="boolean__navbar__menu active">
+                    <li class="boolean__navbar__menu ">
                         <a class="nav-link" href="{{ route('corso') }}">Corso</a>
                     </li>
 
-                    <li class="boolean__navbar__menu active">
+                    <li class="boolean__navbar__menu ">
                         <a class="nav-link" href="#">Dopo il corso</a>
                     </li>
 
-                    <li class="boolean__navbar__menu active">
+                    <li class="boolean__navbar__menu ">
                         <a class="nav-link" href="#">Lezione Gratuita</a>
                     </li>
 
-                    <li class="boolean__navbar__menu active">
+                    <li class="boolean__navbar__menu ">
                         <a class="nav-link" href="#">Assumi i nostri studenti</a>
                     </li>
 
-                    <li class="boolean__navbar__button">
-                        <a href="#">Candidati ora</a>
+                    <li class="boolean__navbar__menu button">
+                        <a class="cta" href="#">Candidati ora</a>
                     </li>
 
                 </ul>
